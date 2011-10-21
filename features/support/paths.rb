0dd8039f075c97c9e1a37aa83a127cf858661946
Michael Drogalis <mjd3089@rit.edu>
2011-10-21 00:02:51 -0400

    Passed first step of Acceptance test. Capybara moves to Sign Up page.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -11,6 +11,8 @@ module NavigationHelpers
 
     when /^the home\s?page$/
       '/'
+    when /^the sign up\s?page$/
+      '/users/sign_up'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
