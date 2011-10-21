d1b9c174678ef274767d891dfd64de1c9dc187af
Michael Drogalis <mjd3089@rit.edu>
2011-10-20 22:16:49 -0400

    Generated Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RunHub::Application.config.secret_token = '4f38b68ba329e9c0a0a18a9563fa025373008c474a589041a8f7974d86d82ff2e7ea483df7d8939f7e8d491f53e31f83479fe10130827b3167231d7411b63b71'
