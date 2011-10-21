6dbd600d4b607d320b727ef88c3a4da2f94c2f70
Michael Drogalis <mjd3089@rit.edu>
2011-10-20 23:31:28 -0400

    Refactored RSpec syntax for readability.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,13 +10,13 @@ describe User do
   describe "user queries" do
     it "should detect an unregistered user" do
       unregistered_username = "rspec-test-user"
-      User.username_exists?(unregistered_username).should == false
+      User.username_exists?(unregistered_username).should be_false
     end
 
     it "should detect a registered user" do
       registered_username = "rspec-registered-user"
       user = Factory(:user)
-      User.username_exists?(registered_username).should == true
+      User.username_exists?(registered_username).should be_true
     end
   end
 end
