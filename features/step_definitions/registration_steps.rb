3c6005160faff146f75f8fd25bd2280c5c9f331e
Michael Drogalis <mjd3089@rit.edu>
2011-10-20 22:39:20 -0400

    Generated pending step definitions for Registration User Story.

---
+++ features/step_definitions/registration_steps.rb
@@ -0,0 +1,16 @@
+Given /^that I am not yet registered$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I register to become a member$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I am taken to my workouts page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see a message related to my registration success$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
