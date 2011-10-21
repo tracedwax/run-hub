37bf386146e879c9ee608e8c28c45adda6b81fde
Michael Drogalis <mjd3089@rit.edu>
2011-10-20 23:17:26 -0400

    Passed RSpec test for unregistered user. First passing Unit Test, woo.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ describe User do
   describe "user queries" do
     it "should detect an unregistered user" do
       unregistered_username = "rspec-test-user"
+      User.username_exists?(unregistered_username).should == false
     end
   end
 end
