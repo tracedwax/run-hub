13f1649db73e48667a00c84a90ea0ffc0336543c
Michael Drogalis <mjd3089@rit.edu>
2011-10-22 22:41:20 -0400

    Moved root route to bottom of routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 RunHub::Application.routes.draw do
   devise_for :users
 
-  root :to => "workouts#index"
-  
   resources :workouts, :only => [:index] do
     member do
       get 'index'
@@ -65,4 +63,6 @@ RunHub::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  root :to => "workouts#index"
 end
