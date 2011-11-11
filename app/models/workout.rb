1f507714f20e9057a8af99a80be6a7b05d37ea20
Michael Drogalis <mjd3089@rit.edu>
2011-11-10 23:40:46 -0500

    Simplified the code by converting to a Time object.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,6 +1,5 @@
 class Workout < ActiveRecord::Base
   def tip_of_week
-    as_date_time = DateTime.new(self.when.year, self.when.month, self.when.day)
-    return as_date_time - as_date_time.wday
+    return self.when.time - self.when.time.wday
   end
 end
