dae8d4bf010add3fdbf364a19a06bc57bbe64960
Michael Drogalis <mjd3089@rit.edu>
2011-11-10 22:37:03 -0500

    Wrote up another passing Unit test. Middle of week calculates its tip.

--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -5,4 +5,9 @@ describe Workout do
     workout = Workout.new :when => Time.new(2011, 11, 6)
     workout.tip_of_week.should == Time.new(2011, 11, 6) 
   end
+
+  it "should calculate the start of the week to be 11/6/11" do
+    workout = Workout.new :when => Time.new(2011, 11, 8)
+    workout.tip_of_week.should == Time.new(2011, 11, 6)
+  end
 end
