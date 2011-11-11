cb64d693e83145394eb6d55567bf03f042e2eff9
Michael Drogalis <mjd3089@rit.edu>
2011-11-10 23:38:32 -0500

    Refactored tip_of_week and removed dead code.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,8 +1,6 @@
 class Workout < ActiveRecord::Base
   def tip_of_week
     as_date_time = DateTime.new(self.when.year, self.when.month, self.when.day)
-    tip = as_date_time - as_date_time.wday
-    
-    return DateTime.new(tip.year, tip.month, tip.day)
+    return as_date_time - as_date_time.wday
   end
 end
