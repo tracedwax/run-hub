cbd136daa85ff18adae3b76d08525a78bffc7a94
Michael Drogalis <mjd3089@rit.edu>
2011-11-13 23:24:01 -0500

    Added User Id to workouts.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111114041930) do
+ActiveRecord::Schema.define(:version => 20111114042339) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20111114041930) do
     t.string   "pace"
     t.string   "route"
     t.text     "notes"
+    t.integer  "user_id"
   end
 
 end
