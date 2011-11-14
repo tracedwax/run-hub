c3a6a5a7963fdbb26ee287efd23340a1bf660962
Michael Drogalis <mjd3089@rit.edu>
2011-11-13 23:14:00 -0500

    Generated a migration for the category of the workout.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111110232417) do
+ActiveRecord::Schema.define(:version => 20111114041244) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20111110232417) do
     t.datetime "when"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "category"
   end
 
 end
