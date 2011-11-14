6189125879060172c88fe19ecf4c3b9b1b1aa6fa
Michael Drogalis <mjd3089@rit.edu>
2011-11-13 23:15:05 -0500

    Generated a migration for the duration of the workout.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111114041244) do
+ActiveRecord::Schema.define(:version => 20111114041424) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20111114041244) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "category"
+    t.string   "duration"
   end
 
 end
