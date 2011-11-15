381e9ac8ba0efc7cb898584ba91b0d4b197e78ef
Michael Drogalis <mjd3089@rit.edu>
2011-11-15 00:47:13 -0500

    Made the default layout the logged in one, made the old one the homepage layout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+  layout :layout_by_resource
+
   def after_sign_up_path_for(resource)
     "/workouts"
+  end
+
+  private
+  def layout_by_resource
+    if devise_controller?
+      "homepage"
+    else
+      "application"
+    end
   end
 end
