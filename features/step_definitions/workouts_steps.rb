a64dbc216b722ede9840d6580ce925b427483835
Michael Drogalis <mjd3089@rit.edu>
2011-11-16 23:37:46 -0500

    wqFixed broken test regarding viewing a workout.

--- features/step_definitions/workouts_steps.rb
+++ features/step_definitions/workouts_steps.rb
@@ -3,14 +3,14 @@ Given /^I have no workouts recorded$/ do
 end
 
 Given /^I recorded a workout on (\d+)\/(\d+)\/(\d+)$/ do |month, day, year|
-  Workout.create! :when => Time.new(year, month, day),
+  Workout.create! :when => DateTime.new(year.to_i, month.to_i, day.to_i),
                   :category => "Easy",
                   :duration => "45:00",
                   :distance => 5.0,
                   :pace => "9:00 min/mile",
                   :route => "Mendon Ponds Park",
                   :notes => "I felt awesome!",
-                  :user_id => 1
+                  :user_id => @user.id
 end
 
 Then /^I should see an empty training week$/ do
@@ -32,7 +32,7 @@ Then /^I should see an empty training week$/ do
 end
 
 Then /^I should see a workout for 11\/6\/2011 listed$/ do
-  within "#workout-week .day#1" do
+  within "#1" do
     assert (page.has_content? "Easy"), "Workout type did not show."
     assert (page.has_content? "45:00"), "Workout duration did not show."
     assert (page.has_content? "5.0"), "Workout distance did not show."
