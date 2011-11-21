145cf715f877e32a58fb507c5e21605a970fd058
Michael Drogalis <mjd3089@rit.edu>
2011-11-20 23:05:59 -0500

    Passed the test.

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ RunHub::Application.routes.draw do
   match 'workouts/create' => 'workouts#create'
   match 'workouts/:startdate' => 'workouts#show_week', :constraints => { :startdate => /\d+-\d+-\d+/ }, :as => :workout
 
-  match 'profiles/:username' => 'profiles#show'
+  match 'profiles/:username' => 'profiles#show', :as => :profile
   match 'profiles/:username/workouts' => 'profiles#show_this_week'
   match 'profiles/:username/workouts/:start_date' => 'profiles#show_week', :constraints => { :start_date => /\d+-\d+-\d+/ }, :as => :public_workouts
 
