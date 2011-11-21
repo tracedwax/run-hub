9ee157cb4bc5019d1658ad44cf2df990718fc3b0
Michael Drogalis <mjd3089@rit.edu>
2011-11-20 23:04:08 -0500

    Wrote a failing Story for following your email address to your public profile.

--- features/step_definitions/profile_steps.rb
+++ features/step_definitions/profile_steps.rb
@@ -1,3 +1,7 @@
 Then /^I should see my username$/ do
   assert (page.has_content? "#{@user.email}"), "Profile did not show username."
 end
+
+When /^I follow the link for my email address in the navigation bar$/ do
+  click_link("#{@user.email}")
+end
