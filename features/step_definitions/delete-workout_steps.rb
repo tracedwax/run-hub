962fd2f433ff25029d948866489aed285a7d2dd3
Michael Drogalis <mjd3089@rit.edu>
2011-11-24 17:37:51 -0500

    Failed the test with my expectations.

--- features/step_definitions/delete-workout_steps.rb
+++ features/step_definitions/delete-workout_steps.rb
@@ -1,8 +1,17 @@
 When /^I delete the workout on 11\/6\/2011$/ do
-  pending # express the regexp above with the code you wish you had
+  within "#1" do
+    click_link "[x]"
+  end
 end
 
 Then /^I should not see a workout for 11\/6\/2011 listed$/ do
-  pending # express the regexp above with the code you wish you had
+  within "#1" do
+    assert (not page.has_content? "Easy"), "Workout type did not show."
+    assert (not page.has_content? "45:00"), "Workout duration did not show."
+    assert (not page.has_content? "5.0"), "Workout distance did not show."
+    assert (not page.has_content? "9:00 min/mile"), "Workout pace did not show."
+    assert (not page.has_content? "Mendon Ponds Park"), "Workout route did not show."
+    assert (not page.has_content? "I felt awesome!"), "Workout notes did not show."
+  end
 end
 
