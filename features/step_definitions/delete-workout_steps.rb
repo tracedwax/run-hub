5b810b83d0a52fc00ed8c0c29d6633e6e5f6657c
Michael Drogalis <mjd3089@rit.edu>
2011-11-24 17:33:47 -0500

    Added the pending steps for the test.

---
+++ features/step_definitions/delete-workout_steps.rb
@@ -0,0 +1,8 @@
+When /^I delete the workout on 11\/6\/2011$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should not see a workout for 11\/6\/2011 listed$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
