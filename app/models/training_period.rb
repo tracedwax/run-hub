1eacddb0ad19607d43690c2fbfcc9c8ea11f95ad
Michael Drogalis <mjd3089@rit.edu>
2011-11-25 15:17:50 -0500

    Passed the Acceptance test.

--- app/models/training_period.rb
+++ app/models/training_period.rb
@@ -63,7 +63,11 @@ class TrainingPeriod
   def total_miles
     self.reduce(0) do |total_miles, day|
       total_miles + day[:workouts].reduce(0) do |daily_miles, current_workout|
-        daily_miles + current_workout.distance
+        if current_workout.distance.nil?
+          daily_miles
+        else
+          daily_miles + current_workout.distance
+        end
       end
     end
   end
